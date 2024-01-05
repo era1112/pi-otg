7cba1c863b40426cb3bc7f4ed49a19bd04e496fd
Era James <51322608+era1112@users.noreply.github.com>
2024-01-04 19:20:35 -0500

    Update gps-cli-120.sh

--- scripts/gps-cli-120.sh
+++ scripts/gps-cli-120.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Set pwd and call payload
+# Set pwd and call script
 cd /home/pi/Documents/gps/src/multi/multi-sdr-gps-sim/
 hackrf_transfer -t /home/pi/Documents/gps/src/gps-sdr-sim/gps-sdr-sim/tapes/paf_loc.bin -f 1575420000 -s 2600000 -a 1 -x 47 		# 120 min tape, needs daily rinex reloaded
