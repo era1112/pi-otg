62d935175cc854eb5154e92b8cd9aae63f6e2f8e
Era James <51322608+era1112@users.noreply.github.com>
2024-01-04 19:06:30 -0500

    Delete payloads/gps-30.sh

--- payloads/gps-30.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Set pwd and call payload
-cd /home/pi/Documents/gps/src/multi/multi-sdr-gps-sim/
-./gps-sim -e rinex/*.23n -l 45.872887484583444,-77.25631500866021,200 -s now -g 47 -a -d 1800 -v -i -r hackrf --disable-almanac		# 30 min tape, needs daily rinex reloaded
